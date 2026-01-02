8e92435a316b6962f274ac99783f6ed4b41ad703
Hari Sehgal <harisehgal34@gmail.com>
2026-01-02 14:25:10 +0530

    cors error fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { partners, productCategories, valueCards, bannerEb2b, footerStatsEb2b
  } from "./init/data.js";
 
 const app = express();
-app.use(cors());
+app.use(cors("*"));
 
 app.get("/", (req, res) => {
   res.send("API is running")
