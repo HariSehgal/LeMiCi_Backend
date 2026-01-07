54bde80ea880c76227c41d0cd6d96ff6e54a2842
Hari Sehgal <harisehgal34@gmail.com>
2026-01-07 09:09:11 +0530

    flag updation

--- routes/index.js
+++ routes/index.js
@@ -11,127 +11,78 @@ import {
   productDetails,
   statsSection,
   softwareReports,
-  softwareCards,
-  supplierData
+  softwareCards
 } from "../init/data.js";
 
 const router = express.Router();
 
-// Feature flags configuration
+// Feature flags 
 const featureFlags = {
-  showCategories: true,
   showBanner1: true,
   showFooterStats: true,
+  showCategories: true,
   showProductCards: true,
-  showSupplierSection: true,
   showBats: true,
   showCompanyProfile: true,
   showProductDetails: true,
   showStatsSection: true,
   showSoftwareReports: true,
-  showSoftwareCards: true,
+  showSoftwareCards: true
 };
 
-// Data endpoints
-router.get("/partners", (req, res) => {
-  res.json(partners);
-});
+// ============ DATA ENDPOINTS ============
+router.get("/partners", (req, res) => res.json(partners));
+router.get("/productCategories", (req, res) => res.json(productCategories));
+router.get("/valueCards", (req, res) => res.json(valueCards));
+router.get("/bannerEb2b", (req, res) => res.json(bannerEb2b));
+router.get("/footerStatsEb2b", (req, res) => res.json(footerStatsEb2b));
+router.get("/productCards", (req, res) => res.json(productCards));
+router.get("/Bats", (req, res) => res.json(Bats));
+router.get("/companyProfile", (req, res) => res.json(companyProfile));
+router.get("/productDetails", (req, res) => res.json(productDetails));
+router.get("/statsSection", (req, res) => res.json(statsSection));
+router.get("/softwareReports", (req, res) => res.json(softwareReports));
+router.get("/softwareCards", (req, res) => res.json(softwareCards));
 
-router.get("/productCategories", (req, res) => {
-  res.json(productCategories);
-});
-
-router.get("/valueCards", (req, res) => {
-  res.json(valueCards);
-});
-
-router.get("/bannerEb2b", (req, res) => {
-  res.json(bannerEb2b);
-});
-
-router.get("/footerStatsEb2b", (req, res) => {
-  res.json(footerStatsEb2b);
-});
-
-router.get("/productCards", (req, res) => {
-  res.json(productCards);
-});
-
-router.get("/Bats", (req, res) => {
-  res.json(Bats);
-});
-
-router.get("/companyProfile", (req, res) => {
-  res.json(companyProfile);
-});
-
-router.get("/productDetails", (req, res) => {
-  res.json(productDetails);
-});
-
-router.get("/statsSection", (req, res) => {
-  res.json(statsSection);
-});
-
-router.get("/softwareReports", (req, res) => {
-  res.json(softwareReports);
-});
-
-router.get("/softwareCards", (req, res) => {
-  res.json(softwareCards);
-});
-
-router.get("/supplierData", (req, res) => {
-  res.json(supplierData);
-});
-
-// Flags endpoints
-router.get("/flags/exploreProducts", (req, res) => {
-  res.json({ showCategories: featureFlags.showCategories });
-});
-
-router.get("/flags/banner", (req, res) => {
+// ============ FLAG ENDPOINTS ============
+router.get("/bannerFlags", (req, res) => {
   res.json({ showBanner1: featureFlags.showBanner1 });
 });
 
-router.get("/flags/footer", (req, res) => {
+router.get("/footerFlags", (req, res) => {
   res.json({ showFooterStats: featureFlags.showFooterStats });
 });
 
-router.get("/flags/productCards", (req, res) => {
+router.get("/exploreProductsFlags", (req, res) => {
+  res.json({ showCategories: featureFlags.showCategories });
+});
+
+router.get("/productCardsFlags", (req, res) => {
   res.json({ showProductCards: featureFlags.showProductCards });
 });
 
-router.get("/flags/supplierSection", (req, res) => {
-  res.json({ showSupplierSection: featureFlags.showSupplierSection });
-});
-
-router.get("/flags/bats", (req, res) => {
+router.get("/BatsFlags", (req, res) => {
   res.json({ showBats: featureFlags.showBats });
 });
 
-router.get("/flags/companyProfile", (req, res) => {
+router.get("/companyProfileFlags", (req, res) => {
   res.json({ showCompanyProfile: featureFlags.showCompanyProfile });
 });
 
-router.get("/flags/productDetails", (req, res) => {
+router.get("/productDetailsFlags", (req, res) => {
   res.json({ showProductDetails: featureFlags.showProductDetails });
 });
 
-router.get("/flags/statsSection", (req, res) => {
+router.get("/statsSectionFlags", (req, res) => {
   res.json({ showStatsSection: featureFlags.showStatsSection });
 });
 
-router.get("/flags/softwareReports", (req, res) => {
+router.get("/softwareReportsFlags", (req, res) => {
   res.json({ showSoftwareReports: featureFlags.showSoftwareReports });
 });
 
-router.get("/flags/softwareCards", (req, res) => {
+router.get("/softwareCardsFlags", (req, res) => {
   res.json({ showSoftwareCards: featureFlags.showSoftwareCards });
-});
-
-router.get("/flags/all", (req, res) => {
-  res.json(featureFlags);
 });
 
 export default router;
